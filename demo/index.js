ccea392fda0ccc376d103d766ba74fa5b3f37acc
liuduanyang <1069831692@qq.com>
2017-12-12 17:00:26 +0800

    demo

--- demo/index.js
+++ demo/index.js
@@ -8,40 +8,33 @@ process.on('uncaughtException',function(err){
 
 var app=connect();
 
-app.use(function(req,res,next){
+app.use('//',function(req,res,next){
     res.setHeader('Content-Type','text/html');
-    res.write(fs.readFileSync(req.url.slice(1)+'.html').toString());
-    res.end();
-})
+    res.write(fs.readFileSync('index.html').toString());
+    res.end();  
+});
 
-app.use('one',start);
-app.use('two',follow);
-app.use('three',end);
-app.use('four',thanks);
-
-function start(req,res,next){
+app.use('/one',function(req,res,next){
     res.setHeader('Content-Type','text/html');
-    res.write(fs.readFileSync(req.url.slice(1)+'.html').toString());
+    res.write(fs.readFileSync('one.html').toString());
     res.end();
-}
+});
 
-function follow(req,res,next){
+app.use('/two',function(req,res,next){
     res.setHeader('Content-Type','text/html');
-    res.write(fs.readFileSync(req.url.slice(1)+'.html').toString());
+    res.write(fs.readFileSync('two.html').toString());
     res.end();
-}
-
-function end(req,res,next){
+});
+app.use('/three',function(req,res,next){
     res.setHeader('Content-Type','text/html');
-    res.write(fs.readFileSync(req.url.slice(1)+'.html').toString());
+    res.write(fs.readFileSync('three.html').toString());
     res.end();
-}
+});
 
-function thanks(req,res,next){
+app.use('/four',function four(req,res,next){
     res.setHeader('Content-Type','text/html');
-    res.write(fs.readFileSync(req.url.slice(1)+'.html').toString());
+    res.write(fs.readFileSync('four.html').toString());
     res.end();
-}
-
+});
 
 http.createServer(app).listen(8080,function(){console.log("服务已启动...")});
